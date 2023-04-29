0c24e2be1a3040c1de8880c9f899cbce2cd88c36
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-04-29 10:49:48 +0200

    avatar + name

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,15 @@ import data from "../data.json";
 
 export default function Home() {
   return (
-    <div className="flex justify-center mx-auto w-full mt-16">
+    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16">
       <Image
         className="rounded-full"
         alt={data.name}
         src={data.avatar}
-        width={80}
-        height={80}
+        width={96}
+        height={96}
       />
-      <h1 className="font-semibold">{data.name}</h1>
+      <h1 className="font-bold mt-4 text-xl">{data.name}</h1>
     </div>
   );
 }

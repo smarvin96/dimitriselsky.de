71c4644a4d8705dc335ffac69d98f8541641a653
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-04-29 12:00:24 +0200

    ui-links

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,19 @@ const LinkCard = ({
   return (
     <a
       href={href}
-      className="flex items-center p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
+      className="flex items-center p-1 w-full rounded-md  hover:scale-105 transition-all border border-gray-300 mb-3"
     >
-      <div className="flex flex-col">
-        <h2 className="font-bold text-lg">{title}</h2>
+      <div className="flex text-center w-full">
+       { image && (
+         <Image
+         className="rounded-sm"
+         alt={title}
+         src={image}
+         width={40}
+         height={40}
+       />
+       )}
+        <h2 className="font-semibold w-full tex-center">{title}</h2>
       </div>
     </a>
   );
@@ -24,7 +33,7 @@ const LinkCard = ({
 
 export default function Home() {
   return (
-    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16">
+    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
       <Image
         className="rounded-full"
         alt={data.name}
@@ -32,7 +41,7 @@ export default function Home() {
         width={96}
         height={96}
       />
-      <h1 className="font-bold mt-4 text-xl">{data.name}</h1>
+      <h1 className="font-bold mt-4 text-xl mb-8">{data.name}</h1>
       {data.links.map((link) => (
         <LinkCard key={link.href} {...link} />
       ))}

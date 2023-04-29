f6b611eb118d3c586453b0f38172ec0280c5d593
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-04-29 10:38:52 +0200

    turbopack statt webpack weil faster

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images : {
+    domains: ['cdn.lnk.bi'],
+  }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
-// import Image from 'next/image'
+import Image from "next/image";
+import data from "../data.json";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold">Hello</h1>
-  )
+    <div className="flex justify-center mx-auto w-full mt-16">
+      <Image
+        className="rounded-full"
+        alt={data.name}
+        src={data.avatar}
+        width={80}
+        height={80}
+      />
+      <h1 className="font-semibold">{data.name}</h1>
+    </div>
+  );
 }

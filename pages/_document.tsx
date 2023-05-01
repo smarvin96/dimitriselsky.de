03595d0b3bc1d2b7819174e2e2109751414ec983
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-05-01 09:22:18 +0200

    bg

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="de">
       <Head />
-      <body className="bg-gradient-to-r from-orange-300 to-rose-300">
+      <body className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
 
         
         <Main />

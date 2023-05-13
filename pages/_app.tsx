f64d8cd863e688b2592c3e100e40b18c15b595bb
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-05-13 08:43:02 +0200

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import '@/components/styles/globals.css'
 
-// import GoogleAnalytics from "@bradgarropy/next-google-analytics"
+import GoogleAnalytics from "@bradgarropy/next-google-analytics"
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-  {/* <GoogleAnalytics measurementId=''/> */}
+  <GoogleAnalytics measurementId='G-NGX2DB4MZ9'/>
   <Component {...pageProps} />
   </>
 }
